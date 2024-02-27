f25b8c19289dfb1e1c4bd39e68930b2e4ff424e5
lzon2024 <161371707+lzon2024@users.noreply.github.com>
2024-02-27 17:47:46 -0300

    Update package.mk

--- packages/sx05re/libretro/tic-80/package.mk
+++ packages/sx05re/libretro/tic-80/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="tic-80"
-PKG_VERSION="074936c04692c8dd8003b6bc6ccd2298d83d0c8b"
+PKG_VERSION="f83a769e63783aac848cb1084450064c16ce0b83"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/nesbox/TIC-80"
-PKG_URL="$PKG_SITE.git"
-PKG_DEPENDS_TARGET="toolchain tic-80:host"
+PKG_URL="${PKG_SITE}.git"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="TIC-80 is a fantasy computer for making, playing and sharing tiny games."
 GET_HANDLER_SUPPORT="git"
 
@@ -14,15 +14,12 @@ PKG_CMAKE_OPTS_TARGET="-DBUILD_LIBRETRO=ON \
 					   -DBUILD_PLAYER=ON \
 					   -DBUILD_DEMO_CARTS=OFF \
                        -DBUILD_SOKOL=OFF \
-                       -DBUILD_SDL=ON"
-
-PKG_CMAKE_OPTS_HOST="-DBUILD_LIBRETRO=OFF \
-					   -DBUILD_PLAYER=OFF \
-					   -DBUILD_DEMO_CARTS=ON \
-                       -DBUILD_SOKOL=OFF \
-                       -DBUILD_SDL=OFF"
+                       -DBUILD_SDL=ON \
+                       -DBUILD_WITH_MRUBY=OFF \
+                       -DCMAKE_BUILD_TYPE=Release \
+                       -DBUILD_WITH_JANET=Off"
 
 makeinstall_target() {
-  mkdir -p $INSTALL/usr/lib/libretro
-  cp $PKG_BUILD/.$TARGET_NAME/lib/tic80_libretro.so $INSTALL/usr/lib/libretro/tic80_libretro.so
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp ${PKG_BUILD}/.${TARGET_NAME}/lib/tic80_libretro.so ${INSTALL}/usr/lib/libretro/tic80_libretro.so
 }
